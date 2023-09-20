74959e2546a836be05dcda59d779e8193d35eaaf
Madhav <maddy595@gmail.com>
2023-09-20 19:13:33 +0530

    FIrst commit

---
+++ src/test/SampleClass1.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SampleClass1 {
+
+	public static void main(String[] args) {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		/*TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+                //after execution, you could see a folder "FailedTestsScreenshots" under src folder
+		String destination = System.getProperty("user.dir") + "/FailedTestsScreenshots/"+screenshotName+dateName+".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source, finalDestination);
+                //Returns the captured file path
+		return destination; */
+		String screenshotNmae="test1";
+//		String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+//		System.out.println(nameofCurrMethod);
+//		String destination = System.getProperty("user.dir") + "\\FailedTestsScreenshots\\"+nameofCurrMethod+dateName+".png";
+//		System.out.println(dateName);
+//		System.out.println(destination);
+		System.out.println("Hello World1");
+	}
+
+}
